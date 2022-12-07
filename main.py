b32882fe8eb10ad15a60919fa72afcee6e950c34
jeffassis <cmdtjeff@gmail.com>
2022-12-07 12:55:09 -0300

    add assets and button GUI

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ texto_admin = Label(tela_principal, text="Administrador", font=("Arial Black",22
 texto_admin.place(x=29, y=95)
 
 #CRIA IMAGEM ADMIN-CRUD
-im_admin_crud = Image.open('assets/admin_crud.png')
-im_admin_crud = im_admin_crud.resize((60,60), Image.ANTIALIAS)
+im_admin_crud = Image.open('assets/admin.png')
+im_admin_crud = im_admin_crud.resize((60,60), Image.Resampling.LANCZOS)
 im_admin_crud = ImageTk.PhotoImage(im_admin_crud)
 #CARREGA IMAGEM ADMIN-CRUD
 l_admin_crud= Label(tela_principal, image=im_admin_crud, compound=LEFT, anchor='nw', bg="#353535",bd=0,activebackground="#3b3b3b")
@@ -57,5 +57,60 @@ e_phone.place(x=120, y=260)
 #ESCOLHA SEXO
 lb_sexo = ttk.Combobox(tela_principal, values=listSexo, font=1)
 lb_sexo.place(x=120,y=300, width=180)
+
+#BOTÕES DE FUNÇÕES CRUD
+#CRIA IMAGEM INSERIR
+im_ml_botao = Image.open('assets/bt_inserir.png')
+im_ml_botao  = im_ml_botao.resize((60,30), Image.Resampling.LANCZOS)
+im_ml_botao  = ImageTk.PhotoImage(im_ml_botao )
+#CARREGA IMAGEM INSERIR - BOTAO INSERIR
+l_ml_botao= Button(tela_principal, image=im_ml_botao, compound=LEFT, anchor='nw', 
+bg="#3b3b3b",bd=0,activebackground="#3b3b3b")
+l_ml_botao.place(x=20,y=349)
+#CRIA IMAGEM DELETAR
+im_ml_botao1 = Image.open('assets/bt_deletar.png')
+im_ml_botao1  = im_ml_botao1.resize((60,30), Image.Resampling.LANCZOS)
+im_ml_botao1  = ImageTk.PhotoImage(im_ml_botao1 )
+#CARREGA IMAGEM DELETAR - BOTAO DELETAR
+l_ml_botao1= Button(tela_principal, image=im_ml_botao1, compound=LEFT, anchor='nw', 
+bg="#3b3b3b",bd=0,activebackground="#3b3b3b")
+l_ml_botao1.place(x=100,y=349)
+#CRIA IMAGEM EDITAR
+im_ml_botao2 = Image.open('assets/bt_editar.png')
+im_ml_botao2  = im_ml_botao2.resize((60,30), Image.Resampling.LANCZOS)
+im_ml_botao2  = ImageTk.PhotoImage(im_ml_botao2 )
+#CARREGA IMAGEM EDITAR - BOTAO EDITAR
+l_ml_botao2= Button(tela_principal, image=im_ml_botao2, compound=LEFT, anchor='nw', 
+bg="#3b3b3b",bd=0,activebackground="#3b3b3b")
+l_ml_botao2.place(x=180,y=349)
+#CRIA IMAGEM BUSCAR
+im_ml_botao3 = Image.open('assets/bt_buscar.png')
+im_ml_botao3  = im_ml_botao3.resize((60,30), Image.Resampling.LANCZOS)
+im_ml_botao3  = ImageTk.PhotoImage(im_ml_botao3 )
+#CARREGA IMAGEM BUSCAR - BOTAO BUSCAR
+l_ml_botao3= Button(tela_principal, image=im_ml_botao3, compound=LEFT, anchor='nw', 
+bg="#3b3b3b",bd=0,activebackground="#3b3b3b")
+l_ml_botao3.place(x=260,y=349)
+
+#PARTE PESQUISA E PAINEL DE DADOS
+#CRIA IMAGEM Pesquisar
+im_ml_pesquisa = Image.open('assets/pesquisab.png')
+im_ml_pesquisa = im_ml_pesquisa.resize((30,30), Image.Resampling.LANCZOS)
+im_ml_pesquisa = ImageTk.PhotoImage(im_ml_pesquisa)
+#CARREGA IMAGEM Pesquisar
+l_ml_pesquisa= Button(tela_principal, image=im_ml_pesquisa, compound=LEFT, anchor='nw', 
+bg="#353535",bd=0,activebackground="#353535")
+l_ml_pesquisa.place(x=460,y=95)
+#PESQUISA
+e_name1 = Entry(tela_principal, width=32,bd=0,bg="#353535",fg="white", insertbackground="white",font=1)
+e_name1.place(x=510, y=95)
+#SEPARADOR
+ttk.Separator(tela_principal, orient=HORIZONTAL).place(x=510,y=120,  width=290)
+#CRIA IMAGEM - LISTA
+im_lista = Image.open('assets/lista.png')
+im_lista = im_lista.resize((40,40), Image.Resampling.LANCZOS)
+im_lista = ImageTk.PhotoImage(im_lista)
+#CARREGA IMAGEM - LISTA
+l_im_lista= Button(tela_principal, image=im_lista, compound=LEFT, anchor='nw', bg="#353535",bd=0,activebackground="#3b3b3b")
 
 root.mainloop()
